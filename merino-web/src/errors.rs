2c45cb274111f9480fd7e46e5dcb08d7c661c153
Michael Cooper <mythmon@gmail.com>
2021-04-12 17:06:21 -0700

    feat(web): Expose suggest crate as a web service

---
+++ merino-web/src/errors.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+
+use actix_web::{HttpResponse, ResponseError, http::StatusCode};
+use serde_json::Value;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum HandlerError  {
+  #[error("Internal error")]
+  Internal
+}
+
+impl ResponseError for HandlerError {
+    fn status_code(&self) -> StatusCode {
+      match self {
+          HandlerError::Internal => StatusCode::INTERNAL_SERVER_ERROR
+      }
+    }
+
+    fn error_response(&self) -> HttpResponse {
+        let mut response = HashMap::new();
+        response.insert(
+            "error".to_owned(),
+            Value::String(format!("{}", self))
+        );
+        HttpResponse::Ok().json(response)
+    }
+}
