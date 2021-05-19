9ecb23d299b41bd2e4516b54c49e8e4609ca60a2
ci-build <ci-build@merino.mozilla.org>
2021-05-19 02:16:54 +0000

    [skip ci] Docs updates

--- merino/docs/sidebar-items.js
+++ merino/docs/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["adrs","Architecture Decision Records for Merino."]]});
+initSidebarItems({"mod":[["adrs","Architecture Decision Records for Merino"],["dev","Developer documentation for working on Merino"],["overview","High level overview of Merino"],["testing","Testing strategies"]]});
