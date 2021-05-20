c91672d3f90e991c961bf4ddba365fe18eb77af1
ci-build <ci-build@merino.mozilla.org>
2021-05-20 23:55:02 +0000

    [skip ci] Docs updates

---
+++ implementors/lazy_static/trait.LazyStatic.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["merino_adm"] = [{"text":"impl <a class=\"trait\" href=\"lazy_static/trait.LazyStatic.html\" title=\"trait lazy_static::LazyStatic\">LazyStatic</a> for <a class=\"struct\" href=\"merino_adm/remote_settings/struct.NON_SPONSORED_IAB_CATEGORIES.html\" title=\"struct merino_adm::remote_settings::NON_SPONSORED_IAB_CATEGORIES\">NON_SPONSORED_IAB_CATEGORIES</a>","synthetic":false,"types":["merino_adm::remote_settings::NON_SPONSORED_IAB_CATEGORIES"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
