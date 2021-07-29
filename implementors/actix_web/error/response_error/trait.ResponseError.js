b71e8fe1ad2bbf57b7ecf3b6b380d89c9604a325
ci-build <ci-build@merino.mozilla.org>
2021-07-29 18:01:39 +0000

    [skip ci] Docs updates

---
+++ implementors/actix_web/error/response_error/trait.ResponseError.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["actix_cors"] = [{"text":"impl <a class=\"trait\" href=\"actix_web/error/response_error/trait.ResponseError.html\" title=\"trait actix_web::error::response_error::ResponseError\">ResponseError</a> for <a class=\"enum\" href=\"actix_cors/enum.CorsError.html\" title=\"enum actix_cors::CorsError\">CorsError</a>","synthetic":false,"types":["actix_cors::error::CorsError"]}];
+implementors["merino_web"] = [{"text":"impl <a class=\"trait\" href=\"actix_web/error/response_error/trait.ResponseError.html\" title=\"trait actix_web::error::response_error::ResponseError\">ResponseError</a> for <a class=\"enum\" href=\"merino_web/errors/enum.HandlerError.html\" title=\"enum merino_web::errors::HandlerError\">HandlerError</a>","synthetic":false,"types":["merino_web::errors::HandlerError"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
