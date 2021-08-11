05b8b3d9420eb04c316503c104233ff694aeb210
Michael Cooper <mythmon@gmail.com>
2021-08-11 13:32:52 -0700

    f review feedback

--- merino/src/docs/api.rs
+++ merino/src/docs/api.rs
@@ -1,9 +1,13 @@
 /*!
 # Merino API documentation
 
-This page describes the API end points available on Merino.
+This page describes the API endpoints available on Merino.
 
 ## Suggest
+
+Endpoint: `/api/v1/suggest`
+
+Example: `/api/v1/suggest?nelson%20mand`
 
 The primary endpoint for the browser to consume, this endpoint consumes user
 input and suggests pages the user may want to visit. The expectation is that
@@ -21,19 +25,31 @@ and headers.
 
 ### Headers
 
-- `Accept-Language` - The language preferences expressed in this header will be
-  honored. Merino will attempt to delivery suggestions in the preferred
-  languages. If none are available, or if Merino does not recognize the
-  languages specified, no results will be returned. Merino will prefer to give
-  suggestions in a language with a lower quality value over giving no responses.
-  Languages with `q=0` or not mentioned in the header will not produce any
-  suggestions.
+- `Accept-Language` - The locale preferences expressed in this header in
+  accordance with [RFC 2616 section 14.4][rfc-2616-14-4] will be used to determine suggestions.
+  Merino maintains a list of supported locales. Merino will choose the
+  locale from it's list that has the highest `q` (quality) value in the
+  user's `Accept-Language` header. Locales with `q=0` will not be used.
+
+  If no locales match, Merino will not return any suggestions. If the
+  header is not included or empty, Merino will default to the `en-US` locale.
+
+  If the highest quality, compatible language produces no suggestion results,
+  Merino will return an empty list instead of attempting to query other
+  languages.
+
+[rfc-2616-14-4]: https://datatracker.ietf.org/doc/html/rfc2616/#section-14.4
 
 ### Other derived inputs
 
 - Location - The IP address of the user or nearest proxy will be used to
   determine location. This location may be as granular as city level, depending
   on server configuration.
+
+  Users that use VPN services will be identified according to the VPN exit
+  node they use, allowing them to change Merino's understanding of their
+  location. VPN exit nodes are often mis-identified in geolocation databases,
+  and may produce unreliable results.
 
 ### Response
 
@@ -43,13 +59,14 @@ following keys:
 
 - `block_id` - a number that can be used, along with the `provider` field below, to
   uniquely identify this suggestion. Two suggestions with the same `provider`
-  and `block_id` may not be byte-for-byte identical, but will be semantically
-  equivalent.
+  and `block_id` should be treated as the same suggestion, even if other
+  fields, such as `click_url` change. Merino will enforce that they are
+  equivalent from a user's point of view.
 
 - `full_keyword` - In the case that the query was a partial match to the
   suggestion, this is the completed query that would also match this query. For
   example, if the user was searching for fruit and typed "appl", this field
-  might contain the string "apples". This is be suitable to show as a completion
+  might contain the string "apples". This is suitable to show as a completion
   of the user's input. This field should be treated as plain text.
 
 - `title` - The full title of the suggestion resulting from the query. Using the
@@ -57,18 +74,19 @@ following keys:
   Northwest". This field should be treated as plain text.
 
 - `url` - The URL of the page that should be navigated to if the user selects
-  this suggestion. This will will be a resource with the title specified in the
+  this suggestion. This will be a resource with the title specified in the
   `title` field.
 
 - `impression_url` - A telemetry URL that should be notified if the browser
   shows this suggestion to the user. This is used along with `click_url` to
   monitor the relevancy of suggestions. For more details see Interaction Pings,
-  below.
+  below. This field may be null, in which case no impression ping is required.
 
 - `click_url` - A telemetry URL that should be notified if the user selects this
   suggestion. This should only be notified as the result of positive user
-  action, and only if the user has navigated to the page specified in the `url`
-  field. For more details see Interaction Pings, below.
+  action, and only if the user has navigated to the page specified in the
+  `url` field. For more details see Interaction Pings, below. This field may
+  be null, in which case no impression ping is required.
 
 - `provider` - A string that identifies the source of this suggestion. This can
   be used along with `block_id` to uniquely identify this suggestion. It is not
@@ -104,6 +122,9 @@ These pings should be delegated to a Mozilla-controlled service instead of being
 sent directly by the browser. This is preferable since it helps maintain user
 privacy.
 
-Requests to the interaction URL should TBD.
+If the URL for a interaction ping is not specified (for example, `click_url`
+is `null`), then no ping should be sent for that action.
+
+The required behavior for interaction pings is TBD.
 
 */
