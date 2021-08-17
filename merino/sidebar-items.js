c8aee4384f46243c4f6d1d34d6a1758c284d43f3
ci-build <ci-build@merino.mozilla.org>
2021-08-17 16:20:38 +0000

    [skip ci] Docs updates

--- merino/sidebar-items.js
+++ merino/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["init_logging","Set up logging for Merino, based on settings and the `RUST_LOG` environment variable."],["main","Primary entry point"]],"mod":[["docs","Merino"]]});
+initSidebarItems({"fn":[["init_logging","Set up logging for Merino, based on settings and the `RUST_LOG` environment variable."],["init_metrics","Set up metrics for Merino, based on settings."],["main","Primary entry point"]],"mod":[["docs","Merino"]]});
--- merino_integration_tests/utils/sidebar-items.js
+++ merino_integration_tests/utils/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["logging","Testing utilities to work with logs."],["redis","Utilities to run tests against a Redis server by using multiple databases to run tests in parallel."],["test_tools","Tools for running tests"]]});
+initSidebarItems({"mod":[["logging","Testing utilities to work with logs."],["metrics","Tools to help testing metrics"],["redis","Utilities to run tests against a Redis server by using multiple databases to run tests in parallel."],["test_tools","Tools for running tests"]]});
