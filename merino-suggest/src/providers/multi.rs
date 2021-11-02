6ead41eb3f5cb7b4e92be00fb26c76ce7ad87467
Michael Cooper <mythmon@gmail.com>
2021-11-02 16:23:45 -0700

    test(suggest): Add tests to verify Multi and IdMulti run providers concurrently
    Fixes #56

--- merino-suggest/src/providers/multi.rs
+++ merino-suggest/src/providers/multi.rs
@@ -63,3 +63,98 @@ impl SuggestionProvider for Multi {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        CacheStatus, Multi, SuggestError, SuggestionProvider, SuggestionRequest, SuggestionResponse,
+    };
+    use async_trait::async_trait;
+    use fake::{Fake, Faker};
+    use tokio::sync::oneshot::error::TryRecvError;
+
+    /// A provider that can be externally paused mid-request.
+    struct ChannelProvider {
+        tx: tokio::sync::mpsc::Sender<()>,
+        rx: tokio::sync::Mutex<tokio::sync::mpsc::Receiver<()>>,
+    }
+
+    #[async_trait]
+    impl SuggestionProvider for ChannelProvider {
+        fn name(&self) -> String {
+            "channel".to_string()
+        }
+
+        async fn suggest(
+            &self,
+            _request: SuggestionRequest,
+        ) -> Result<SuggestionResponse, SuggestError> {
+            self.tx.send(()).await.unwrap();
+            self.rx.lock().await.recv().await.unwrap();
+
+            Ok(SuggestionResponse {
+                cache_status: CacheStatus::NoCache,
+                cache_ttl: None,
+                suggestions: vec![],
+            })
+        }
+    }
+
+    #[tokio::test]
+    async fn multi_is_concurrent() {
+        // Set up two way communication for two internal providers, and a one shot provider to get the suggestion out of a thread.
+        let (prov1_input_tx, prov1_input_rx) = tokio::sync::mpsc::channel::<()>(4);
+        let (prov1_output_tx, mut prov1_output_rx) = tokio::sync::mpsc::channel::<()>(4);
+        let (prov2_input_tx, prov2_input_rx) = tokio::sync::mpsc::channel::<()>(4);
+        let (prov2_output_tx, mut prov2_output_rx) = tokio::sync::mpsc::channel::<()>(4);
+        let (suggestion_result_tx, mut suggestion_result_rx) =
+            tokio::sync::oneshot::channel::<()>();
+
+        // Set up the providers
+        let prov1 = ChannelProvider {
+            tx: prov1_output_tx,
+            rx: tokio::sync::Mutex::new(prov1_input_rx),
+        };
+        let prov2 = ChannelProvider {
+            tx: prov2_output_tx,
+            rx: tokio::sync::Mutex::new(prov2_input_rx),
+        };
+        let multi = Multi::new(vec![Box::new(prov1), Box::new(prov2)]);
+
+        // Start a request that will use both prov1 and prov2 via a multi provider.
+        let task_handle = tokio::spawn(async move {
+            let request: SuggestionRequest = Faker.fake();
+            multi.suggest(request).await.unwrap();
+            // signal that the request has finished
+            suggestion_result_tx.send(()).unwrap();
+        });
+
+        // Confirm that both providers have called and have sent a message over
+        // their output channel (requesting to continue). This is the most
+        // important assertion, as it demonstrates that both providers have
+        // started before either of them have finished.
+        tokio::join!(prov1_output_rx.recv(), prov2_output_rx.recv());
+
+        // Make sure no response has been received
+        assert!(matches!(
+            suggestion_result_rx.try_recv(),
+            Err(TryRecvError::Empty)
+        ));
+
+        // Allow one provider to continue
+        prov1_input_tx.send(()).await.unwrap();
+
+        // Make sure no response has been received
+        assert!(matches!(
+            suggestion_result_rx.try_recv(),
+            Err(TryRecvError::Empty)
+        ));
+
+        // Allow the other provider to continue
+        prov2_input_tx.send(()).await.unwrap();
+
+        // Wait for the response.
+        suggestion_result_rx.await.unwrap();
+        task_handle.await.unwrap();
+    }
+}
