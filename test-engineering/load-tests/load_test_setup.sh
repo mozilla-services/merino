3406243d8e073f3d2cec4a281b4d9860a4eb7a71
Ankita Shrivastava <62558650+ashrivastava-qa@users.noreply.github.com>
2021-11-18 14:12:50 -0500

    Update test-engineering/load-tests/load_test_setup.sh
    Co-authored-by: Peter deHaan <peter@deseloper.com>

--- test-engineering/load-tests/load_test_setup.sh
+++ test-engineering/load-tests/load_test_setup.sh
@@ -80,7 +80,7 @@ $KUBECTL apply -f $MERINO_DIRECTORY/$WORKER_FILE
 echo -e "==================== Verify the Locust deployments & Services"
 $KUBECTL get pods -o wide
 $KUBECTL get services
-sleep 30s
+sleep 30
 
 EXTERNAL_IP=$($KUBECTL get svc locust-master -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
 
