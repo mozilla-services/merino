aea156bb9ecd7649d18a1504ad8660de82f0dc4b
Ankita Shrivastava <ashrivastava@mozilla.com>
2021-11-19 13:36:22 -0500

    add review changes

--- test-engineering/load-tests/load_test_setup.sh
+++ test-engineering/load-tests/load_test_setup.sh
@@ -6,7 +6,6 @@ GCLOUD=$(which gcloud)
 SED=$(which sed)
 KUBECTL=$(which kubectl)
 
-#RED_OUPUT='\033[0;31m'
 DIRECTORY=$(pwd)
 SDK_DIRECTORY=google-cloud-sdk
 MERINO_DIRECTORY=$DIRECTORY/kubernetes-config
@@ -28,7 +27,6 @@ TARGET=stage.merino.nonprod.cloudops.mozgcp.net
 SCOPE="https://www.googleapis.com/auth/cloud-platform"
 REGION=us-central1
 ZONE=${REGION}-b
-PROJECT=spheric-keel-331521
 
 #Configure Kubernetes
 echo -e "==================== Prepare environments with set of environment variables "
@@ -38,10 +36,6 @@ echo -e "==================== Set Kubernetes TARGET "
 export TARGET=$TARGET
 echo -e "==================== Set SCOPE "
 export SCOPE=$SCOPE
-
-echo -e "==================== Prepare GCP by updating default zone and project ID "
-$GCLOUD config set compute/zone $ZONE
-$GCLOUD config set project $PROJECT
 
 echo -e "==================== Refresh Kubeconfig at path ~/.kube/config "
 $GCLOUD container clusters get-credentials $CLUSTER --zone $ZONE --project $PROJECT
@@ -55,25 +49,16 @@ $GCLOUD container images list | grep locust-merino
 ##Deploying the Locust master and worker nodes
 echo -e "==================== Update Kubernetes Manifests "
 echo -e "==================== Replace the target host and project ID with the deployed endpoint and project ID in the locust-master-controller.yml and locust-worker-controller.yml files"
-$SED -i -e "s/\[TARGET_HOST\]/$TARGET/g" $MERINO_DIRECTORY/$MASTER_FILE
-$SED -i -e "s/\[TARGET_HOST\]/$TARGET/g" $MERINO_DIRECTORY/$WORKER_FILE
-$SED -i -e "s/\[PROJECT_ID\]/$PROJECT/g" $MERINO_DIRECTORY/$MASTER_FILE
-$SED -i -e "s/\[PROJECT_ID\]/$PROJECT/g" $MERINO_DIRECTORY/$WORKER_FILE
-$SED -i -e "s/\[LOCUST_IMAGE_TAG\]/$LOCUST_IMAGE_TAG/g" $MERINO_DIRECTORY/$MASTER_FILE
-$SED -i -e "s/\[LOCUST_IMAGE_TAG\]/$LOCUST_IMAGE_TAG/g" $MERINO_DIRECTORY/$WORKER_FILE
-$SED -i -e "s/\[KINTO_BUCKET\]/$KINTO_BUCKET/g" $MERINO_DIRECTORY/$MASTER_FILE
-$SED -i -e "s/\[KINTO_BUCKET\]/$KINTO_BUCKET/g" $MERINO_DIRECTORY/$WORKER_FILE
-$SED -i -e "s/\[KINTO_COLLECTION\]/$KINTO_COLLECTION/g" $MERINO_DIRECTORY/$MASTER_FILE
-$SED -i -e "s/\[KINTO_COLLECTION\]/$KINTO_COLLECTION/g" $MERINO_DIRECTORY/$WORKER_FILE
-$SED -i -e "s/\[KINTO_SERVER_URL\]/$KINTO_SERVER_URL/g" $MERINO_DIRECTORY/$MASTER_FILE
-$SED -i -e "s/\[KINTO_SERVER_URL\]/$KINTO_SERVER_URL/g" $MERINO_DIRECTORY/$WORKER_FILE
-$SED -i -e "s/\[LOCUST_IMAGE_TAG\]/$LOCUST_IMAGE_TAG/g" $MERINO_DIRECTORY/$WORKER_FILE
-$SED -i -e "s/\[KINTO_BUCKET\]/$KINTO_BUCKET/g" $MERINO_DIRECTORY/$MASTER_FILE
-$SED -i -e "s/\[KINTO_BUCKET\]/$KINTO_BUCKET/g" $MERINO_DIRECTORY/$WORKER_FILE
-$SED -i -e "s/\[KINTO_COLLECTION\]/$KINTO_COLLECTION/g" $MERINO_DIRECTORY/$MASTER_FILE
-$SED -i -e "s/\[KINTO_COLLECTION\]/$KINTO_COLLECTION/g" $MERINO_DIRECTORY/$WORKER_FILE
-$SED -i -e "s/\[KINTO_SERVER_URL\]/$KINTO_SERVER_URL/g" $MERINO_DIRECTORY/$MASTER_FILE
-$SED -i -e "s/\[KINTO_SERVER_URL\]/$KINTO_SERVER_URL/g" $MERINO_DIRECTORY/$WORKER_FILE
+FILES=($MASTER_FILE $WORKER_FILE)
+for file in "${FILES[@]}"
+do
+    $SED -i -e "s/\[TARGET_HOST\]/$TARGET/g" $MERINO_DIRECTORY/$file
+    $SED -i -e "s/\[PROJECT_ID\]/$GOOGLE_CLOUD_PROJECT/g" $MERINO_DIRECTORY/$file
+    $SED -i -e "s/\[LOCUST_IMAGE_TAG\]/$LOCUST_IMAGE_TAG/g" $MERINO_DIRECTORY/$file
+    $SED -i -e "s/\[KINTO_BUCKET\]/$KINTO_BUCKET/g" $MERINO_DIRECTORY/$file
+    $SED -i -e "s/\[KINTO_COLLECTION\]/$KINTO_COLLECTION/g" $MERINO_DIRECTORY/$file
+    $SED -i -e "s/\[KINTO_SERVER_URL\]/$KINTO_SERVER_URL/g" $MERINO_DIRECTORY/$file
+done
 
 $KUBECTL delete pods --all
 $KUBECTL delete svc locust-master
@@ -87,9 +72,3 @@ $KUBECTL apply -f $MERINO_DIRECTORY/$WORKER_FILE
 echo -e "==================== Verify the Locust deployments & Services"
 $KUBECTL get pods -o wide
 $KUBECTL get services
-sleep 30
-
-EXTERNAL_IP=$($KUBECTL get svc locust-master -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
-
-echo -e "==================== Get the external IP address of the system: $EXTERNAL_IP"
-echo -e "==================== Open your browser and then open the Locust master web interface http://$EXTERNAL_IP:8089"
