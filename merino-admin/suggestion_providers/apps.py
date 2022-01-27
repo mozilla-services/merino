76e8729a7ee934cb0347e4dade49d08c1589d516
tiftran <ttran@mozilla.com>
2022-01-27 13:01:30 -0800

    added client_variant_switch to merino-admin

--- merino-admin/suggestion_providers/apps.py
+++ merino-admin/suggestion_providers/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class SuggestionProvidersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'suggestion_providers'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "suggestion_providers"
