ff318c33b920a5bde79672cbe4c4ae21e6f3395f
Nan Jiang <njiang028@gmail.com>
2022-04-26 13:10:47 -0400

    chore: Remove merino-shepherd to favor consvc-shepherd [do not deploy]

--- merino-shepherd/merino_suggestion_providers/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SuggestionProvidersConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "merino_suggestion_providers"
--- merino-shepherd/settings.py
+++
@@ -1,4 +0,0 @@
-INSTALLED_APPS = [
-    "merino_suggestion_providers",
-    "django.contrib.contenttypes",
-]
