16a4c1e9f50ba42bfaef85e0121540e7156a25f5
Katrina Anderson <3422688+Trinaa@users.noreply.github.com>
2022-09-08 17:05:26 -0400

    test: clear records between scenario executions in contract tests [do not deploy] * add teardown fixture in merino contract tests * add delete records function to kinto module * increase delay in contract test scenarios to assure remote settings resync in merino * update README

--- test-engineering/contract-tests/client/tests/test_merino.py
+++ test-engineering/contract-tests/client/tests/test_merino.py
@@ -9,7 +9,13 @@ import pytest
 import requests
 from requests import Response as RequestsResponse
 
-from kinto import KintoEnvironment, KintoRequestRecord, upload_attachment, upload_icons
+from kinto import (
+    KintoEnvironment,
+    KintoRequestRecord,
+    delete_records,
+    upload_attachment,
+    upload_icons,
+)
 from models import (
     KintoRequest,
     MerinoRequest,
@@ -185,6 +191,15 @@ def assert_200_response(
             expected_suggestion = expected_suggestions_by_id[suggestion_id(suggestion)]
             assert suggestion.icon == expected_suggestion.icon
             continue
+
+
+@pytest.fixture(scope="function", autouse=True)
+def fixture_function_teardown(kinto_environment: KintoEnvironment):
+    """Execute instructions after each test."""
+
+    yield  # Allow test to execute
+
+    delete_records(kinto_environment)
 
 
 def test_merino(steps: List[Step], step_functions: Dict[Service, Callable]):
